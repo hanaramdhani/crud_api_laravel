4f9a27bfd3f7d3079807ef4f00d1136e95c50ef6
Hana Ramdhani <hanawesave@gmail.com>
2021-07-05 08:43:20 +0800

    uo

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('password', function () {
+    return bcrypt('hana');
+});
+
+
 Route::get('categories', 'API\categoriesController@index');
 Route::get('categories/{id}', 'API\categoriesController@show');
 Route::post('categories', 'API\categoriesController@store');
 Route::delete('categories/{id}', 'API\categoriesController@destroy');
 Route::patch('categories/{id}', 'API\categoriesController@update');
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
